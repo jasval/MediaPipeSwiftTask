be5c9387d83c64cf6602b595a8a2d1438391e1bb
Jasper <28489757+jasval@users.noreply.github.com>
2025-06-01 14:46:46 +0100

    Update binary URLs to v1.0.3 release

--- Package.swift
+++ Package.swift
@@ -23,12 +23,12 @@ let package = Package(
         ),
         .binaryTarget(
             name: "MediaPipeTasksGenAI",
-            url: "https://github.com/jasval/MediaPipeSwiftTask/releases/download/v1.0.1/MediaPipeTasksGenAI.xcframework.zip",
+            url: "https://github.com/jasval/MediaPipeSwiftTask/releases/download/v1.0.3/MediaPipeTasksGenAI.xcframework.zip",
             checksum: "76b850b0e66554f9c0adf249e41ba2ee1f22fa057e9c0e94c8a98a610e3b9a01"
         ),
         .binaryTarget(
             name: "MediaPipeTasksGenAIC", 
-            url: "https://github.com/jasval/MediaPipeSwiftTask/releases/download/v1.0.1/MediaPipeTasksGenAIC.xcframework.zip",
+            url: "https://github.com/jasval/MediaPipeSwiftTask/releases/download/v1.0.3/MediaPipeTasksGenAIC.xcframework.zip",
             checksum: "0a639941dbcb4b16f85ca2b1412ed7c97a7c9020fb592095023507ded34fd688"
         )
     ]
